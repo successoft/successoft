0c6bbc74891680f645780384457edc07531b06e2
Antônio Roberto  Silva <forevertonny@gmail.com>
2010-12-29 15:44:50 -0200

    first config, thanks tomas ;)

---
+++ boot.rb
@@ -0,0 +1,18 @@
+period_dir = File.expand_path('.')
+$LOAD_PATH.unshift(period_dir) unless $LOAD_PATH.include?(period_dir)
+
+begin
+  require File.expand_path(".bundle/environment", __FILE__)
+rescue LoadError
+  require "rubygems"
+  require "bundler"
+
+  Bundler.setup(:default)
+end
+
+Bundler.require
+
+require 'config/environment'
+Configuration.config do |config|
+  config.load_paths = %w(. app/helpers app/views app/requests)
+end
---
+++ config/environment.rb
@@ -0,0 +1,18 @@
+class Configuration
+  # call block in the scope of configuration instance
+  #
+  def self.config
+    yield Configuration.new
+  end
+
+  # Pass many strings that will be include in the load path and require
+  # all ruby files in the specified dir
+  #
+  def load_paths=(dirs)
+    dirs.each do |dir|
+      directory = File.expand_path(dir)
+      $LOAD_PATH.unshift(directory) unless $LOAD_PATH.include?(directory)
+      Dir["#{directory}/*.rb"].each { |file| require file }
+    end
+  end  
+end
