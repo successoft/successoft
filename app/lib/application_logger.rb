55f723056137c41163546f1d687fcc9ef0cf022a
Antônio Roberto  Silva <forevertonny@gmail.com>
2010-12-29 19:08:17 -0200

    logger

---
+++ app/lib/application_logger.rb
@@ -0,0 +1,9 @@
+module ApplicationLogger
+  def logger_file!
+    logger_filename = "logs/successoft.log"
+    logger_file = File.open(logger_filename, 'a+')
+    logger_file.sync = true
+    DataMapper::Logger.new(logger_file, :debug)
+    logger_file
+  end
+end
