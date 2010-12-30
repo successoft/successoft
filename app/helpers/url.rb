d659076a1918579968ccc55f043dcf17baff2e94
Antônio Roberto  Silva <forevertonny@gmail.com>
2010-12-30 21:01:38 -0200

    helpers

---
+++ app/helpers/url.rb
@@ -0,0 +1,19 @@
+module Helpers
+  module Url
+    def root_path
+      "/" 
+    end
+    
+    def about_path
+      "/about"
+    end
+    
+    def contact_path
+      "/contact"
+    end
+    
+    def services_path
+      "/services"
+    end
+  end  
+end
---
+++ spec/helpers/url_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+describe Sinatra::Application do
+  include Rack::Test::Methods
+  include Helpers::Url
+
+  def app
+    @app ||= Sinatra::Application
+  end
+
+  describe 'test routes path' do
+    
+    it "root_path should be equal to /" do
+      root_path.should == '/'
+    end
+    
+    %w(about services contact).each do |page|
+      it "#{page}_path should be equal to /#{page}" do
+        send("#{page}_path").should == "/#{page}"        
+      end
+    end
+  end
+end
