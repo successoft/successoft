1c7077dab9156e4204a5d0e30771c827b28c7e5b
Tomas D'Stefano <tomas_stefano@successoft.com>
2012-09-23 21:16:25 -0300

    Change the email to send on contact form

--- app/requests/successoft.rb
+++ app/requests/successoft.rb
@@ -1,55 +1,55 @@
 class SuccesSoft
   extend ApplicationLogger
-  
+
   use Rack::Flash
   use Rack::ShowExceptions
   use Rack::CommonLogger, logger_file!
-  
+
   set :root, File.expand_path(File.join(File.dirname(__FILE__), '..', '..'))
   set :views, Proc.new { File.join(root, 'app', "views") }
   set :public, Proc.new { File.join(root, "public") }
-  set :logging, false     
-  
+  set :logging, false
+
   enable :sessions
-  
-  helpers do    
-    include ActionView::Helpers   
+
+  helpers do
+    include ActionView::Helpers
     include Helpers
     include Helpers::Url
-  end 
-  
-  get '/' do 
-    @title = I18n.t("titles.home")    
+  end
+
+  get '/' do
+    @title = I18n.t("titles.home")
     haml :index
   end
-  
-  post '/sent_mail' do         
+
+  post '/sent_mail' do
     mail = Mail.new
-    
+
     mail[:from] = params["Email"]
-    mail[:to] = 'forevertonny@gmail.com'
+    mail[:to] = 'contato@successoft.com'
     mail[:subject] = params["Assunto"]
     mail[:body] = """
                     #{params["Mensagem"]}
                     Enviado por: #{params["Email"]}
                   """
     begin
-      if mail.deliver!    
+      if mail.deliver!
         flash[:notice] = I18n.t("messages.notices.contact")
-      else                                                                 
+      else
         flash[:alert] = I18n.t("messages.alerts.contact")
       end
     rescue
-      flash[:alert] = I18n.t("messages.alerts.contact")      
+      flash[:alert] = I18n.t("messages.alerts.contact")
     end
-    
+
     redirect '/contact'
   end
-  
+
   %w(index about products contact).each do |page|
     get "/#{page}" do
       @title = I18n.t("titles.#{page}")
       haml page.to_sym
     end
-  end 
+  end
 end
